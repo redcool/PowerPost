9a1210a288c122ecc8970da25123e9b312625b13
ken <redcool007@qq.com>
2023-02-27 15:02:55 +0800

    v(2.0.4.3) remove unused ns

--- PowerPostPrj/PowerPost/Framework/Animation/CodeGen/PowerPostKeyframeAnimGen.cs
+++ PowerPostPrj/PowerPost/Framework/Animation/CodeGen/PowerPostKeyframeAnimGen.cs
@@ -1,19 +1,17 @@
 ﻿#if UNITY_EDITOR
 using PowerPost;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor;
 using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
-using Codice.CM.SEIDInfo;
-using System.Text.RegularExpressions;
 
-namespace PowerUtilities { 
+namespace PowerUtilities
+{
     public class PowerPostKeyframeAnimGen
     {
         const string PATH = "Assets/PowerPostAnimCodeGen/";
